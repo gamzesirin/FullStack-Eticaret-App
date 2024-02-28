3b2b8d7861e8984b5f85abf1abc3def3beaa2c93
Gamze Şirin <gamze1sirin@gmail.com>
2024-02-28 17:23:19 +0300

    finish project.

---
+++ frontend/src/components/Categories/CategoryItem.jsx
@@ -0,0 +1,20 @@
+import './CategoryItem.css'
+
+import PropTypes from 'prop-types'
+
+const CategoryItem = ({ category }) => {
+	return (
+		<li className="category-item">
+			<a href="#">
+				<img src={category?.img} alt className="category-image" />
+				<span className="category-title">{category?.name}</span>
+			</a>
+		</li>
+	)
+}
+
+export default CategoryItem
+
+CategoryItem.propTypes = {
+	category: PropTypes.object
+}
